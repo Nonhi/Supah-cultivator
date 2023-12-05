95e3ede83246a4b26a70bb64964b0266a473d890
Nonhi <88535776+Nonhi@users.noreply.github.com>
2023-12-05 23:31:25 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -6,10 +6,29 @@ var QiPerClickCost = 10
 var ManualAmount = 0
 var ManualCost = 4
 var BreakthroughStage = 1
-var BreakthroughCost = 1000  
+var BreakthroughCost = 1000 
+var BreakthroughMult = 1
 var RebirthCost = 10000
 var RebirthStage = 1
+var lastTick = Date.now()
+var Health = 100
+var Attack = 50
+var Soulshards = 0
  
+var Thug = {
+    Hp: 50,
+    Soulshard:1
+}
+
+function fightThug() {
+    
+    Thug.Hp -= Attack
+    if (Thug.Hp <= 0) {
+        Soulshards += Thug.Soulshard
+        document.getElementById("Soulshards").innerHTML = Soulshards + " soulshards"
+    }
+    
+}
 
 
 function cultivate() {
@@ -34,18 +53,26 @@ function buyManual() {
         ManualAmount += (1*RebirthStage)
         ManualCost *= 2
         document.getElementById("qicultivated").innerHTML = Qi + " Qi"
-        document.getElementById("ManualUpgrade").innerHTML = "Buy cultivation manual (Level " + ManualAmount + ") Cost: " + ManualCost +" Qi"
+        document.getElementById("ManualUpgrade").innerHTML = "Buy cultivation manual (Level " + (ManualAmount/RebirthStage)+ ") Cost: " + ManualCost +" Qi"
 
+    }
+}
+
+function buyIbumetin() {
+    if (Soulshards >= 1000) {
+        localStorage.removeItem("SupahcultivatorSave")
+        location.reload()
     }
 }
 
 function Breakthrough() {
     if (Qi >= BreakthroughCost) {
         Qi -= BreakthroughCost
-        BreakthroughStage += (1*RebirthStage)
+        BreakthroughStage += 1
+        BreakthroughMult += (1*RebirthStage)
         BreakthroughCost *= 2
         document.getElementById("qicultivated").innerHTML = Qi + " Qi"
-        document.getElementById("BreakthroughSystem").innerHTML = "Break through to realm" + BreakthroughStage + "Cost: " + BreakthroughCost + " Qi"
+        document.getElementById("BreakthroughSystem").innerHTML = "Break through to next realm (Level "+ (BreakthroughStage) + ") Cost: " + BreakthroughCost + " Qi"
 
     }
 }
@@ -62,6 +89,7 @@ function Rebirth() {
         ManualAmount = 0
         ManualCost = 4
         BreakthroughStage = 1
+        BreakthroughMult = 1
         BreakthroughCost = 1000 
         document.getElementById("qicultivated").innerHTML = Qi + " Qi";
         document.getElementById("RebirthSystem").innerHTML = "Rebirth" + " (level " + RebirthStage + ") Cost: " + RebirthCost + " Qi";
@@ -73,9 +101,17 @@ function Rebirth() {
 }
 
 function PassiveQiGain() {
-    Qi += (((ManualAmount + QiPerClick)*(BreakthroughStage*RebirthStage)));
+    Qi += (((ManualAmount + QiPerClick)*(BreakthroughMult*RebirthStage)));
     document.getElementById("qicultivated").innerHTML = Qi + " Qi"
-    document.getElementById("PassiveGain").innerHTML = (((ManualAmount + QiPerClick)*(BreakthroughStage*RebirthStage))) + " Qi/s"
+    document.getElementById("PassiveGain").innerHTML = (((ManualAmount + QiPerClick)*(BreakthroughMult*RebirthStage))) + " Qi/s"
+}
+
+function tab(tab) {
+    document.getElementById("cultivateMenu").style.display = "none";
+    document.getElementById("shopMenu").style.display = "none";
+    document.getElementById("combatMenu").style.display = "none";
+    document.getElementById("statsMenu").style.display = "none";
+    document.getElementById(tab).style.display = "inline-block";
 }
 
 function Save() {
@@ -87,9 +123,13 @@ function Save() {
         ManualAmount: ManualAmount,
         ManualCost: ManualCost,
         BreakthroughStage: BreakthroughStage,
+        BreakthroughMult: BreakthroughMult,
         BreakthroughCost: BreakthroughCost,
         RebirthStage: RebirthStage,
-        RebirthCost: RebirthCost
+        RebirthCost: RebirthCost, 
+        Health:Health,
+        Attack:Attack,
+        Soulshards:Soulshards 
     }
     localStorage.setItem("SupahcultivatorSave", JSON.stringify(gamesave))
 }
@@ -101,12 +141,8 @@ function resetData() {
 
 
 
-function ShowTooltip() {
-    
-}
-
 var mainGameLoop = window.setInterval(function() {
-    PassiveQiGain() 
+    PassiveQiGain()
 }, 1000)
 
 
@@ -126,14 +162,23 @@ function loadgame() {
     if (typeof savedgame.RebirthStage!== "undefined") RebirthStage = savedgame.RebirthStage;
     if (typeof savedgame.RebirthCost!== "undefined") RebirthCost= savedgame.RebirthCost;
     if (typeof savedgame.QiPerClickLevel!== "undefined") QiPerClickLevel= savedgame.QiPerClickLevel;
+    if (typeof savedgame.lastTick !== "undefined") lastTick = savedgame.lastTick;
+    if (typeof savedgame.BreakthroughMult !== "undefined") BreakthroughMult = savedgame.BreakthroughMult;
+    if (typeof savedgame.Attack !== "undefined") Attack = savedgame.Attack;
+    if (typeof savedgame.Health !== "undefined") Health = savedgame.Health;
+    if (typeof savedgame.Soulshards !== "undefined") Soulshards = savedgame.Soulshards;
 }
 
 window.onload = function() {
     loadgame();
-    document.getElementById("ManualUpgrade").innerHTML = "Buy cultivation manual (Level " + ManualAmount + ") Cost: " + ManualCost +" Qi";
+    tab("cultivateMenu");
+    document.getElementById("ManualUpgrade").innerHTML = "Buy cultivation manual (Level " + (ManualAmount/RebirthStage) + ") Cost: " + ManualCost +" Qi";
     document.getElementById("PassiveGain").innerHTML = (ManualAmount + QiPerClick) + " Qi/s";
     document.getElementById("perClickUpgrade").innerHTML = "Strenghten soul realm (Level " + QiPerClickLevel + ") Cost: " + QiPerClickCost + " Qi";
-    document.getElementById("BreakthroughSystem").innerHTML = "Break through to next realm "+ "(Level " + BreakthroughStage + ")" + " Cost: " + BreakthroughCost + " Qi";
+    document.getElementById("BreakthroughSystem").innerHTML = "Break through to next realm "+ "(Level " + (BreakthroughStage) + ")" + " Cost: " + BreakthroughCost + " Qi";
     document.getElementById("qicultivated").innerHTML = Qi + " Qi";
     document.getElementById("RebirthSystem").innerHTML = "Rebirth" + "(level" + RebirthStage + ")" + "Cost: " + RebirthCost + " Qi";
+    document.getElementById("Health").innerHTML = "Health: " + Health;
+    document.getElementById("Attack").innerHTML = "Attack: " + Attack;
+    document.getElementById("Soulshards").innerHTML = Soulshards + " soulshards"
 }
